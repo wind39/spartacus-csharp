8d362285f5a71838ba424b0c97734d6853919249
wind39 <william.ivanski@gmail.com>
2014-10-21 17:12:14 -0200

    0.31.12: Mesclada classe Spartacus.Utils.CSVTable dentro da classe Spartacus.Utils.Excel.

--- Spartacus/Spartacus.Utils.Excel.cs
+++ Spartacus/Spartacus.Utils.Excel.cs
@@ -1,8 +1,5 @@
 using System;
 
-//TODO: Mesclar classe CSVTable na classe Excel.
-//TODO: Criar função para Exportação.
-
 namespace Spartacus.Utils
 {
     /// <summary>
@@ -60,24 +57,130 @@ namespace Spartacus.Utils
         /// <summary>
         /// Importa todas as planilhas de um arquivo Excel para várias <see cref="System.Data.DataTable"/> dentro de um <see cref="System.Data.DataSet"/>.
         /// </summary>
-        /// <param name="p_filename">Nome do arquivo XLSX a ser importado.</param>
+        /// <param name="p_filename">Nome do arquivo XLSX ou CSV a ser importado.</param>
         /// <exception cref="Spartacus.Utils.Exception">Exceção acontece quando não conseguir ler o arquivo de origem, ou quando ocorrer qualquer problema na SejExcel.</exception>
         public void Import(string p_filename)
         {
-            Spartacus.ThirdParty.SejExcel.OoXml v_package;
-            Spartacus.ThirdParty.SejExcel.gSheet v_sheet;
             System.IO.FileInfo v_fileinfo;
+            Spartacus.Utils.File v_file;
 
             v_fileinfo = new System.IO.FileInfo(p_filename);
+
             if (! v_fileinfo.Exists)
             {
                 throw new Spartacus.Utils.Exception(string.Format("Arquivo {0} nao existe.", p_filename));
             }
+            else
+            {
+                try
+                {
+                    v_file = new Spartacus.Utils.File(1, 1, Spartacus.Utils.FileType.FILE, p_filename);
+
+                    switch (v_file.v_extension.ToLower())
+                    {
+                        case "xlsx":
+                            this.ImportXLSX(p_filename);
+                            break;
+                        case "csv":
+                            this.ImportCSV(p_filename, ';', System.Text.Encoding.Default);
+                            break;
+                        default:
+                            throw new Spartacus.Utils.Exception("Extensão {0} desconhecida.", v_file.v_extension.ToLower());
+                    }
+                }
+                catch (Spartacus.Utils.Exception e)
+                {
+                    throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                }
+                catch (System.Exception e)
+                {
+                    throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Importa todas as planilhas de um arquivo Excel para várias <see cref="System.Data.DataTable"/> dentro de um <see cref="System.Data.DataSet"/>.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo XLSX ou CSV a ser importado.</param>
+        /// <param name="p_separator">Separador de campos do arquivo CSV.</param>
+        /// <param name="p_encoding">Codificação para leitura do arquivo CSV.</param>
+        /// <exception cref="Spartacus.Utils.Exception">Exceção acontece quando não conseguir ler o arquivo de origem, ou quando ocorrer qualquer problema na SejExcel.</exception>
+        public void Import(string p_filename, char p_separator, System.Text.Encoding p_encoding)
+        {
+            System.IO.FileInfo v_fileinfo;
+            Spartacus.Utils.File v_file;
+
+            v_fileinfo = new System.IO.FileInfo(p_filename);
+
+            if (! v_fileinfo.Exists)
+            {
+                throw new Spartacus.Utils.Exception(string.Format("Arquivo {0} nao existe.", p_filename));
+            }
+            else
+            {
+                try
+                {
+                    v_file = new Spartacus.Utils.File(1, 1, Spartacus.Utils.FileType.FILE, p_filename);
+
+                    switch (v_file.v_extension.ToLower())
+                    {
+                        case "xlsx":
+                            this.ImportXLSX(p_filename);
+                            break;
+                        case "csv":
+                            this.ImportCSV(p_filename, p_separator, p_encoding);
+                            break;
+                        default:
+                            throw new Spartacus.Utils.Exception("Extensão {0} desconhecida.", v_file.v_extension.ToLower());
+                    }
+                }
+                catch (Spartacus.Utils.Exception e)
+                {
+                    throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                }
+                catch (System.Exception e)
+                {
+                    throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Importa uma lista de arquivos Excel.
+        /// A lista pode conter arquivos XLSX ou CSV, e pode ser misturado.
+        /// </summary>
+        /// <param name="p_filelist">Lista de nomes de arquivos.</param>
+        public void Import(System.Collections.ArrayList p_filelist)
+        {
+            try
+            {
+                foreach (string v_filename in p_filelist)
+                    this.Import(v_filename);
+            }
+            catch (Spartacus.Utils.Exception e)
+            {
+                throw e;
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Importa todas as planilhas de um arquivo XLSX para várias <see cref="System.Data.DataTable"/> dentro de um <see cref="System.Data.DataSet"/>.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo XLSX.</param>
+        private void ImportXLSX(string p_filename)
+        {
+            Spartacus.ThirdParty.SejExcel.OoXml v_package;
+            Spartacus.ThirdParty.SejExcel.gSheet v_sheet;
 
             try
             {
                 v_package = new Spartacus.ThirdParty.SejExcel.OoXml(p_filename);
-                
+
                 if (v_package != null && v_package.sheets != null && v_package.sheets.Count > 0)
                 {
                     foreach (string v_key in v_package.sheets.Keys)
@@ -90,11 +193,83 @@ namespace Spartacus.Utils
             }
             catch (Spartacus.Utils.Exception e)
             {
-                throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                throw e;
             }
             catch (System.Exception e)
             {
-                throw new Spartacus.Utils.Exception("Erro ao converter para DataSet o arquivo {0}.", e, p_filename);
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Importa um arquivo CSV para um <see cref="System.Data.DataTable"/>.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo CSV.</param>
+        /// <param name="p_separator">Separador de campos do arquivo CSV.</param>
+        /// <param name="p_encoding">Codificação para leitura do arquivo CSV.</param>
+        private void ImportCSV(string p_filename, char p_separator, System.Text.Encoding p_encoding)
+        {
+            Spartacus.Utils.File v_file;
+            System.IO.StreamReader v_reader = null;
+            System.Data.DataTable v_table;
+            System.Data.DataRow v_row;
+            string[] v_line;
+            int i, j;
+
+            try
+            {
+                v_file = new Spartacus.Utils.File(1, 1, Spartacus.Utils.FileType.FILE, p_filename);
+                v_table = new System.Data.DataTable(v_file.v_name.Replace("." + v_file.v_extension, ""));
+
+                v_reader = new System.IO.StreamReader(p_filename, p_encoding);
+
+                i = 0;
+                while (! v_reader.EndOfStream)
+                {
+                    v_line = v_reader.ReadLine().Split(p_separator);
+
+                    if (i == 0)
+                    {
+                        for (j = 0; j < v_line.Length; j++)
+                            v_table.Columns.Add(v_line [j]);
+                    }
+                    else
+                    {
+                        if (v_line.Length != v_table.Columns.Count)
+                        {
+                            throw new Spartacus.Utils.Exception("Linha {0} contém {1} colunas, diferente do esperado.", i, v_line.Length);
+                        }
+                        else
+                        {
+                            v_row = v_table.NewRow();
+                            for (j = 0; j < v_table.Columns.Count; j++)
+                                v_row[j] = v_line[j];
+                            v_table.Rows.Add(v_row);
+                        }
+                    }
+
+                    i++;
+                }
+
+                v_reader.Close();
+
+                this.v_set.Tables.Add(v_table);
+            }
+            catch (Spartacus.Utils.Exception e)
+            {
+                throw e;
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (v_reader != null)
+                {
+                    v_reader.Close();
+                    v_reader = null;
+                }
             }
         }
 
@@ -299,6 +474,133 @@ namespace Spartacus.Utils
             }
 
             throw new Spartacus.Utils.Exception("Controle de coluna {0} nao existe na lista de controles de colunas.", v_columncontrol);
+        }
+
+        /// <summary>
+        /// Exporta todas as <see cref="System.Data.DataTable"/> dentro de um <see cref="System.Data.DataSet"/> para um arquivo Excel.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo XLSX ou CSV a ser exportado.</param>
+        /// <exception cref="Spartacus.Utils.Exception">Exceção acontece quando não conseguir escrever no arquivo de destino, ou quando ocorrer qualquer problema na SejExcel.</exception>
+        /// <remarks>No caso de arquivo CSV, apenas a primeira tabela do DataSet será salva.</remarks>
+        public void Export(string p_filename)
+        {
+            Spartacus.Utils.File v_file;
+
+            v_file = new Spartacus.Utils.File(1, 1, Spartacus.Utils.FileType.FILE, p_filename);
+
+            try
+            {
+                switch (v_file.v_extension.ToLower())
+                {
+                    case "xlsx":
+                        this.ExportXLSX(p_filename);
+                        break;
+                    case "csv":
+                        this.ExportCSV(p_filename, ';', System.Text.Encoding.Default);
+                        break;
+                    default:
+                        throw new Spartacus.Utils.Exception("Extensão {0} desconhecida.", v_file.v_extension.ToLower());
+                }
+            }
+            catch (Spartacus.Utils.Exception e)
+            {
+                throw new Spartacus.Utils.Exception("Erro ao salvar o DataSet no arquivo {0}.", e, p_filename);
+            }
+            catch (System.Exception e)
+            {
+                throw new Spartacus.Utils.Exception("Erro ao salvar o DataSet no arquivo {0}.", e, p_filename);
+            }
+        }
+
+        /// <summary>
+        /// Exporta todas as <see cref="System.Data.DataTable"/> dentro de um <see cref="System.Data.DataSet"/> para um arquivo Excel.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo XLSX ou CSV a ser exportado.</param>
+        /// <param name="p_separator">Separador de campos do arquivo CSV.</param>
+        /// <param name="p_encoding">Codificação de escrita do arquivo CSV.</param>
+        /// <exception cref="Spartacus.Utils.Exception">Exceção acontece quando não conseguir escrever no arquivo de destino, ou quando ocorrer qualquer problema na SejExcel.</exception>
+        /// <remarks>No caso de arquivo CSV, apenas a primeira tabela do DataSet será salva.</remarks>
+        public void Export(string p_filename, char p_separator, System.Text.Encoding p_encoding)
+        {
+            Spartacus.Utils.File v_file;
+
+            v_file = new Spartacus.Utils.File(1, 1, Spartacus.Utils.FileType.FILE, p_filename);
+
+            try
+            {
+                switch (v_file.v_extension.ToLower())
+                {
+                    case "xlsx":
+                        this.ExportXLSX(p_filename);
+                        break;
+                    case "csv":
+                        this.ExportCSV(p_filename, p_separator, p_encoding);
+                        break;
+                    default:
+                        throw new Spartacus.Utils.Exception("Extensão {0} desconhecida.", v_file.v_extension.ToLower());
+                }
+            }
+            catch (Spartacus.Utils.Exception e)
+            {
+                throw new Spartacus.Utils.Exception("Erro ao salvar o DataSet no arquivo {0}.", e, p_filename);
+            }
+            catch (System.Exception e)
+            {
+                throw new Spartacus.Utils.Exception("Erro ao salvar o DataSet no arquivo {0}.", e, p_filename);
+            }
+        }
+
+        /// <summary>
+        /// Exporta todas as <see cref="System.Data.DataTable"/> de um <see cref="System.Data.DataSet"/> para um arquivo XLSX.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo XLSX.</param>
+        private void ExportXLSX(string p_filename)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Exporta a primeira <see cref="System.Data.DataTable"/> de um <see cref="System.Data.DataSet"/> para um arquivo CSV.
+        /// </summary>
+        /// <param name="p_filename">Nome do arquivo CSV.</param>
+        private void ExportCSV(string p_filename, char p_separator, System.Text.Encoding p_encoding)
+        {
+            System.IO.StreamWriter v_writer = null;
+            string v_text;
+            int i;
+
+            try
+            {
+                v_writer = new System.IO.StreamWriter(new System.IO.FileStream(p_filename, System.IO.FileMode.Create), p_encoding);
+
+                v_text = this.v_set.Tables[0].Columns[0].ColumnName;
+                for (i = 1; i < this.v_set.Tables[0].Columns.Count; i++)
+                    v_text += p_separator + this.v_set.Tables[0].Columns[i].ColumnName;
+                v_writer.WriteLine(v_text);
+
+                foreach (System.Data.DataRow r in this.v_set.Tables[0].Rows)
+                {
+                    v_text = r[0].ToString();
+                    for (i = 1; i < this.v_set.Tables[0].Columns.Count; i++)
+                        v_text += p_separator + r[i].ToString().Replace(';', ',');
+                    v_writer.WriteLine(v_text);
+                }
+
+                v_writer.Flush();
+                v_writer.Close();
+            }
+            catch (System.Exception e)
+            {
+                throw new Spartacus.Utils.Exception(e);
+            }
+            finally
+            {
+                if (v_writer != null)
+                {
+                    v_writer.Close();
+                    v_writer = null;
+                }
+            }
         }
     }
 }
