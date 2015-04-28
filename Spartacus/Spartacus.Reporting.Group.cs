36a37f1230fe8180cd8262c46d8f67b733901da4
wind39 <william.ivanski@gmail.com>
2015-04-28 16:28:56 -0300

    0.37.1:  Corrigido bug na renderização de cabeçalhos de grupo na Spartacus.Reporting

--- Spartacus/Spartacus.Reporting.Group.cs
+++ Spartacus/Spartacus.Reporting.Group.cs
@@ -125,7 +125,7 @@ namespace Spartacus.Reporting
             // adicionando todas as colunas do cabeçalho do grupo
             for (k = 0; k < this.v_headerfields.Count; k++)
             {
-                if (((Spartacus.Reporting.Field)this.v_headerfields [k]).v_column != "")
+                if (! v_allcolumns_temp.Contains(((Spartacus.Reporting.Field)this.v_headerfields [k]).v_column) && ((Spartacus.Reporting.Field)this.v_headerfields [k]).v_column != "")
                     v_allcolumns_temp.Add(((Spartacus.Reporting.Field)this.v_headerfields [k]).v_column);
             }
 
