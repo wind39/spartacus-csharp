8580a86c1bf339a4f102dc304259a937f517a0e4
wind39 <william.ivanski@gmail.com>
2015-05-14 00:23:41 -0300

    0.38.1:  Permitindo caractere # na Spartacus.Database.Command

--- Spartacus/Spartacus.Database.Command.cs
+++ Spartacus/Spartacus.Database.Command.cs
@@ -120,7 +120,7 @@ namespace Spartacus.Database
 			string v_newstring;
 			int i, j, k;
 			char[][] v_handler = new char[56][];
-			char[] v_allowed = new char[85];
+			char[] v_allowed = new char[86];
 			bool v_achou;
 			char[] v_newarray;
 
@@ -271,6 +271,7 @@ namespace Spartacus.Database
             v_allowed[82] = '>';
             v_allowed[83] = '[';
             v_allowed[84] = ']';
+            v_allowed[85] = '$';
 
 			v_newarray = v_newstring.ToCharArray();
 			for (i = 0; i < v_newarray.Length; i++)
@@ -308,7 +309,7 @@ namespace Spartacus.Database
             string v_newstring;
             int i, j, k;
             char[][] v_handler = new char[56][];
-            char[] v_allowed = new char[86];
+            char[] v_allowed = new char[87];
             bool v_achou;
             char[] v_newarray;
 
@@ -460,6 +461,7 @@ namespace Spartacus.Database
             v_allowed[83] = '>';
             v_allowed[84] = '[';
             v_allowed[85] = ']';
+            v_allowed[86] = '$';
 
             v_newarray = v_newstring.ToCharArray();
             for (i = 0; i < v_newarray.Length; i++)
@@ -497,7 +499,7 @@ namespace Spartacus.Database
             string v_newstring;
             int i, j, k;
             char[][] v_handler = new char[56][];
-            char[] v_allowed = new char[89];
+            char[] v_allowed = new char[90];
             bool v_achou;
             char[] v_newarray;
 
@@ -652,6 +654,7 @@ namespace Spartacus.Database
             v_allowed[86] = '%';
             v_allowed[87] = '"';
             v_allowed[88] = '\\';
+            v_allowed[89] = '$';
 
             v_newarray = v_newstring.ToCharArray();
             for (i = 0; i < v_newarray.Length; i++)
