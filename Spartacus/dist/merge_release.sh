3cd30a19553b322297e3de2004b975a833887c81
William Ivanski <william.ivanski@gmail.com>
2016-02-24 17:20:19 -0300

    0.41.1:  Adicionando Mono.Security.dll para funcionar acesso a PostgreSQL no Windows

--- Spartacus/dist/merge_release.sh
+++ Spartacus/dist/merge_release.sh
@@ -1,5 +1,5 @@
 ﻿#! /bin/bash
 
-dist/ILRepack.exe /out:dist/Spartacus.dll bin/Release/Spartacus.dll bin/Release/FirebirdSql.Data.FirebirdClient.dll bin/Release/Mono.Data.Sqlite.dll bin/Release/Mono.Data.Tds.dll bin/Release/MySql.Data.dll bin/Release/Npgsql.dll bin/Release/Oracle.ManagedDataAccess.dll
+dist/ILRepack.exe /out:dist/Spartacus.dll bin/Release/Spartacus.dll bin/Release/FirebirdSql.Data.FirebirdClient.dll bin/Release/Mono.Data.Sqlite.dll bin/Release/Mono.Data.Tds.dll bin/Release/MySql.Data.dll bin/Release/Npgsql.dll bin/Release/Oracle.ManagedDataAccess.dll bin/Release/Mono.Security.dll
 rm -f bin/Release/*.dll
 cp dist/Spartacus.dll bin/Release/
